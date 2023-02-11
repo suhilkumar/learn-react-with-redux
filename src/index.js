293c215fb6a2c0d2d7688604ca263dc1ec854ef0
Suhilkumar <suhilkumarpatel@gmail.com>
2023-02-11 14:49:38 -0600

    add expression

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDom.createRoot(element);
+
+const App = (props) => {
+  let message = `bye there`;
+  if (Math.random() > 0.5) {
+    message = "Hi there";
+  }
+  return (
+    <>
+      <h1>{message}</h1>
+      {/* you can write expressions in curly brackets as well */}
+      <h3>{new Date().toLocaleTimeString()}</h3>
+    </>
+  );
+};
+root.render(<App />);
